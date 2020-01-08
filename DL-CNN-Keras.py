0d8184529dd3bb6967b65b22a55965dbc75616de
Pranav Raikote <43779710+pranavraikote@users.noreply.github.com>
2020-01-08 23:16:05 +0530

    Update DL-CNN-Keras.py

--- DL-CNN-Keras.py
+++ DL-CNN-Keras.py
@@ -54,9 +54,9 @@ _, acc = model.evaluate(testX, testY)
 print("Accuracy is {}%".format(acc*100))
 
 #Predictions for the sample images
-a = model.predict(trainX[20:21])
-b = model.predict(trainX[21:22])
-c = model.predict(trainX[22:23])
-print(a.argmax())
-print(b.argmax())
-print(c.argmax())
+a = model.predict_classes(trainX[20:21])[0]
+b = model.predict_classes(trainX[21:22])[0]
+c = model.predict_classes(trainX[22:23])[0]
+print(a)
+print(b)
+print(c)
